9caaa75e220619ecaf2f012171da8ed031f29736
Anders Skaret Johansen <anders.skaret.johansen@gmail.com>
2025-09-11 10:26:45 +0800

    fevopen

--- node1/main.c
+++ node1/main.c
@@ -8,6 +8,9 @@ int main() {
     //flashingLED();
     
     uart_init();
+    fdevopen(*uart_transmit, *uart_receive);
+    printf("Hei");
+
     while(1){
         uart_transmit('a');
         for(uint32_t i = 0; i < 400000; ++i);
