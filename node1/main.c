0773a2263a4969edbeaa5345d7529b28eded0d1c
Anders Sandvik <anders-01@live.no>
2025-10-02 14:19:26 +0200

    noeTestCanCont

--- node1/main.c
+++ node1/main.c
@@ -8,6 +8,7 @@
 #include "display.h"
 #include "ioboard.h"
 #include "graphics.h"
+#include "cancont.h"
 
 
 void dataModel(void){
@@ -25,5 +26,24 @@ int main() {
     spiMasterInit();
 
     dispInit();
-    testText();
+    printf("hi\n\r");
+
+    cancontInit();
+    #define TXB0D0 0b00110110
+    while(1){
+        spiChipSelect(spiCAN);
+        spiMasterTransmit(0b00000011); //Ready read
+        spiMasterTransmit(TXB0D0);
+        char data = spiMasterReceive();
+        spiChipSelect(spiOff);
+        for (uint16_t i = 0; i < 10000; i++);
+    }
+    
+
+    //#define TXB0D0 0b00110110
+    //cancontInit();
+    //cancontWrite(20,TXB0D0);
+    //char data = cancontRead(TXB0D0);
+
+    //printf("%i\n\r",data);
 }
